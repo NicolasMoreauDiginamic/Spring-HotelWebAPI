1aeab2386320580116c847b08c5ab669702dd1ca
Nicolas Moreau <nicolas.moreau93@orange.fr>
2022-02-01 14:52:38 +0100

    commit TP HotelWebAPI GetClient et PostClient

--- src/main/java/dev/hotel/controller/ClientController.java
+++ src/main/java/dev/hotel/controller/ClientController.java
@@ -1,8 +1,11 @@
 package dev.hotel.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,18 @@ public class ClientController {
 		this.clientServ = clientServ;
 	}
 	
-	@GetMapping
+	@GetMapping("client/?start=X&size=Y")
 	public List<Client> listeClient(){
 		return this.clientServ.listClient();
+	}
+	
+	@GetMapping("client/{id}")
+	public Optional<Client> SearchClient(Client client) {
+		return this.clientServ.SearchClientByID(client.getId());
+	}
+	
+	@PostMapping
+	public Client nouveauClient(@RequestBody Client client) {
+		return this.clientServ.newClient(client);
 	}
 }
--- src/main/java/dev/hotel/services/ClientService.java
+++ src/main/java/dev/hotel/services/ClientService.java
@@ -1,8 +1,12 @@
 package dev.hotel.services;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import dev.hotel.entite.Client;
 import dev.hotel.repositories.ClientRepository;
@@ -20,5 +24,13 @@ public class ClientService {
 		return this.clientRepo.findAll();
 	}
 	
+	public Optional<Client> SearchClientByID(@PathVariable Integer id) {
+		return this.clientRepo.findById(id);
+	}
+	
+	@Transactional
+	public Client newClient(Client client) {
+		return this.clientRepo.save(client);
+	}
 	
 }
